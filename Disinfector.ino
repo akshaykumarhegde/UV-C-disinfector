0ee3dc560dce5ea0780fa08e0c7fb93e334d3c25
akshaykumarhegde <37041712+akshaykumarhegde@users.noreply.github.com>
2020-09-26 21:06:28 +0530

    Update Disinfector.ino

--- Disinfector.ino
+++ Disinfector.ino
@@ -1,42 +1,145 @@
-#define echoPin 2 // attach pin D2 Arduino to pin Echo of HC-SR04
-#define trigPin 3 //attach pin D3 Arduino to pin Trig of HC-SR04
+#include "SevenSegmentTM1637.h"
+const byte PIN_CLK = 9;   // define CLK pin (any digital pin)
+const byte PIN_DIO = 8;
 #define santctrl 4
-long duration;
-int distance; 
+int buttonState1 = 0;
+int buttonState2 = 0;
+int lastButtonState1 = 0;    
+int lastButtonState2 = 0;
+int buttonPin1 =6;
+int buttonPin2=7;
+int count = 1;
+int count1;
+int state;
+int ctrl;
+int preval;
+long previousMillis;
+
+SevenSegmentTM1637    display(PIN_CLK, PIN_DIO);
+
+const int  IRbuttonPin3 = 2; 
+const int  IRpump = 3; 
+//int buttonPushCounter3 = 0;   // counter for the number of button presses
+int IRbuttonState3 = 0;         // current state of the button
+int IRlastButtonState3 = 0;     // previous state of the button
+int IRsense;
+long IRinterval = 1000;
+int IRsanictrl;
+unsigned long IRpreviousMillis = 0; 
+
 
 void setup() {
-  pinMode(santctrl, OUTPUT);
-  pinMode(trigPin, OUTPUT);
-  pinMode(echoPin, INPUT);
-  Serial.begin(9600); 
-  Serial.println("Ultrasonic Sensor HC-SR04 Test");
-  Serial.println("with Arduino UNO R3");
-}
-void loop(){
-  sanitiser();
-  
-  }
-void sanitiser() {
-  // Clears the trigPin condition
-  digitalWrite(trigPin, LOW);
-  delayMicroseconds(2);
-  // Sets the trigPin HIGH (ACTIVE) for 10 microseconds
-  digitalWrite(trigPin, HIGH);
-  delayMicroseconds(10);
-  digitalWrite(trigPin, LOW);
-  // Reads the echoPin, returns the sound wave travel time in microseconds
-  duration = pulseIn(echoPin, HIGH);
-  // Calculating the distance
-  distance = duration * 0.034 / 2; // Speed of sound wave divided by 2 (go and back)
-  // Displays the distance on the Serial Monitor
-  Serial.print("Distance: ");
-  Serial.print(distance);
-  Serial.println(" cm");
-  if (distance < 5) {
-  digitalWrite(santctrl, HIGH);//statement()
-  delay(200);
-  digitalWrite(santctrl, LOW);
+      display.begin();
+  digitalWrite(A0, HIGH);
+  Serial.print("oning"); 
+  pinMode(LED_BUILTIN, OUTPUT );
+  pinMode(A0, OUTPUT);
+  pinMode(buttonPin1, INPUT);
+  pinMode(buttonPin2, INPUT);
+  // initialize the button pin as a input:
+  pinMode(IRbuttonPin3, INPUT);
+  pinMode(IRpump,OUTPUT); 
+  // initialize the LED as an output:
+  // initialize serial communication:
+  Serial.begin(9600);
 }
 
-  
+
+void contro() {
+  IRbuttonState3 = digitalRead(IRbuttonPin3);
+  if (IRbuttonState3 != IRlastButtonState3) {
+    if (IRbuttonState3 == HIGH) {
+      Serial.println("on");
+       digitalWrite(IRpump, LOW);
+    } else {
+      Serial.println("off");
+      IRsanictrl=0;
+  }
+  IRlastButtonState3 = IRbuttonState3;
+}
+}
+
+void loop(){
+  contro();
+    mode();
+  select();
+  millitmr();
+   if(IRsanictrl == 0){
+      Serial.println("led on");
+      digitalWrite(IRpump, HIGH);
+        IRsanictrl++;}
+        unsigned long IRcurrentMillis = millis();
+        if(IRcurrentMillis - IRpreviousMillis > IRinterval) {
+       IRpreviousMillis = IRcurrentMillis;
+       digitalWrite(IRpump, LOW); 
+    }
+ }
+ void millitmr(){
+  if(state == 1){
+    if(ctrl == 0){
+      Serial.println("led on");
+         previousMillis = millis();
+         ctrl++;
+    } 
+         digitalWrite(LED_BUILTIN, HIGH); 
+         unsigned long currentMillis = millis();
+         if(currentMillis - previousMillis > count1){
+         Serial.println("led off");
+         digitalWrite(LED_BUILTIN, LOW);
+          delay(50);
+         state++;
+          Serial.println("reboot");
+          display.clear();
+          display.print(" ov");
+         digitalWrite(A0, LOW);
+  }
+  }
+}
+void mode() {
+  buttonState2 = digitalRead(buttonPin2);
+  if (buttonState2 != lastButtonState2) {
+    if (buttonState2 == HIGH) {
+      //Serial.println("step2off");
+    } 
+    else {
+      Serial.println("step2on");
+       count++;
+         //Serial.println(count);
+         
+        }
+    delay(50);
+  }
+  lastButtonState2 = buttonState2;
+   if(count == 1){
+        count1 = 2000;
+           preval=2;}
+          if(count == 2){
+        count1 = 5000;
+           preval=5;}
+        if(count == 3){
+        count1 = 8000;
+           preval=8;}
+        if(count == 4){
+        count = 1;}
+        display.print(preval);
+//        display.clear();
+        //Serial.println(count); 
+}
+void select() {
+  buttonState1 = digitalRead(buttonPin1);
+  if (buttonState1 != lastButtonState1) {
+    if (buttonState1 == HIGH) {
+     // Serial.println("step1off");
+     //state++;
+    }
+    else {
+      Serial.println("step10n");
+      state++;
+      if(state == 2){
+        state = 0;
+        }
+    }
+    delay(50);
+  }
+  lastButtonState1 = buttonState1;
 }
